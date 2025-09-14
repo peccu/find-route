e749512b05b8d991c038edab1252c9b7d61ef03c
peccu <peccul+github@gmail.com>
2025-09-14 22:12:03 +0900

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,3 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("route-planner-cache-v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.webmanifest",
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
