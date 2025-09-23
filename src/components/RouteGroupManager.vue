e81526d261905d355fdca88cdd7190782ee4c67d
peccu <peccul@gmail.com>
2025-09-23 09:09:47 +0900

    fix style

--- src/components/RouteGroupManager.vue
+++ src/components/RouteGroupManager.vue
@@ -1,23 +1,14 @@
 <template>
   <div class="bg-white p-4 rounded shadow">
 
-    <CustomSelector
-      v-model="selectedGroup"
-      :items="routeGroups"
-      label-key="name"
-      value-key="name"
-      label="経路グループを選択"
-      placeholder="選択してください"
-    />
+    <CustomSelector v-model="selectedGroup" :items="routeGroups" label-key="name" value-key="name" label="経路グループを選択"
+      placeholder="選択してください" />
     <div class="flex items-center my-3 gap-2">
       <button @click="creatingGroup = true" class="px-2 py-1 bg-green-500 text-white rounded">
         グループ追加
       </button>
-      <button
-        v-if="selectedGroup"
-        @click="removeGroup(selectedGroup.id)"
-        class="px-2 py-1 bg-red-400 text-white rounded"
-      >
+      <button v-if="selectedGroup" @click="removeGroup(selectedGroup.id)"
+        class="px-2 py-1 bg-red-400 text-white rounded">
         グループ削除
       </button>
     </div>
@@ -29,23 +20,18 @@
 
       <div class="space-y-1">
         <div>グループ名</div>
-        <input
-          v-model="newGroupName"
-          placeholder="グループ名"
-          class="border px-2 py-1 rounded"
-        />
+        <input v-model="newGroupName" placeholder="グループ名" class="border px-2 py-1 rounded" />
       </div>
 
       <div class="space-y-1">
         <div>
           位置設定
-          <a v-if="newLat" target="_blank" class="underline text-blue-800" :href="`https://maps.google.com/?q=(${newLat}, ${newLng})`">Google Mapsで確認</a>
-          <a v-else target="_blank" class="underline text-blue-800" href="https://maps.google.com/">Google Mapsで探す</a>
+          <a v-if="newLat" target="_blank" class="underline text-blue-800"
+            :href="`https://maps.google.com/?q=(${newLat}, ${newLng})`">Google Mapsで確認</a>
+          <a v-else target="_blank" class="underline text-blue-800" href="https://maps.google.com/">Google
+            Mapsで探す</a>
         </div>
-        <LocationInput
-          v-model:modelValueLat="newLat"
-          v-model:modelValueLng="newLng"
-        />
+        <LocationInput v-model:modelValueLat="newLat" v-model:modelValueLng="newLng" />
       </div>
 
       <div class="space-x-2">
@@ -64,28 +50,19 @@
 
       <div class="space-y-1">
         <div>グループ名</div>
-        <input
-          v-model="selectedGroup.name"
-          placeholder="グループ名"
-          class="border px-2 py-1 rounded"
-        />(自動保存)
+        <input v-model="selectedGroup.name" placeholder="グループ名" class="border px-2 py-1 rounded" />(自動保存)
       </div>
 
       <div class="space-y-1">
         <div>位置設定</div>
-        <LocationInput
-          v-model:modelValueLat="lat"
-          v-model:modelValueLng="lng"
-        />
-        <a v-if="lat" target="_blank" class="underline text-blue-800" :href="`https://maps.google.com/?q=(${lat}, ${lng})`">Google Mapsで確認</a>
-        <a v-else target="_blank" class="underline text-blue-800" href="https://maps.google.com/">Google Mapsで探す</a>
+        <LocationInput v-model:modelValueLat="lat" v-model:modelValueLng="lng" />
+        <a v-if="lat" target="_blank" class="underline text-blue-800"
+          :href="`https://maps.google.com/?q=(${lat}, ${lng})`">Google Mapsで確認</a>
+        <a v-else target="_blank" class="underline text-blue-800" href="https://maps.google.com/">Google
+          Mapsで探す</a>
       </div>
 
-      <RouteManager
-        v-if="selectedGroup"
-        :routes="selectedGroup.routes"
-        @update:routes="updateRoutes"
-      />
+      <RouteManager v-if="selectedGroup" :routes="selectedGroup.routes" @update:routes="updateRoutes" />
 
     </div>
 
@@ -101,10 +78,10 @@ import type { RouteGroup, Route } from '../types'
 import { uid } from '../utils'
 
 const props = defineProps<{
-    routeGroups: RouteGroup[]
+  routeGroups: RouteGroup[]
 }>()
 const emit = defineEmits<{
-    (e: 'update:routeGroups', value: RouteGroup[]): void
+  (e: 'update:routeGroups', value: RouteGroup[]): void
 }>()
 
 const selectedGroup = ref<RouteGroup | null>(null)
@@ -127,21 +104,21 @@ function addGroup() {
 }
 
 function updateRoutes(routes: Route[]) {
-    if (!selectedGroup.value) return;
-    const list = props.routeGroups.map(group =>
-        (selectedGroup.value && group.id === selectedGroup.value.id)
-            ? { ...group, routes }
-            : group
-    )
-    emit('update:routeGroups', list)
+  if (!selectedGroup.value) return;
+  const list = props.routeGroups.map(group =>
+    (selectedGroup.value && group.id === selectedGroup.value.id)
+      ? { ...group, routes }
+      : group
+  )
+  emit('update:routeGroups', list)
 }
 
 function removeGroup(id: string) {
-    if (!confirm('このグループを削除しますか？')) return
-    const list = props.routeGroups.filter(group => group.id !== id)
-    if (selectedGroup.value && selectedGroup.value.id === id) {
-        selectedGroup.value = null
-    }
-    emit('update:routeGroups', list)
+  if (!confirm('このグループを削除しますか？')) return
+  const list = props.routeGroups.filter(group => group.id !== id)
+  if (selectedGroup.value && selectedGroup.value.id === id) {
+    selectedGroup.value = null
+  }
+  emit('update:routeGroups', list)
 }
 </script>
